16bff32c57983881beb8548906833a18baecb83b
Rony Bubnovsky <rony.bubnovsky@gmail.com>
2025-03-13 02:02:34 +0200

    style: enhance game message display on ChessBoard component

--- components/ChessBoard.tsx
+++ components/ChessBoard.tsx
@@ -450,8 +450,8 @@ export default function ChessBoard({
       )}
 
       {gameMessage && (
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center z-20">
-          <h1 className="text-white text-2xl font-bold text-center p-4">
+        <div className="absolute -top-10 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white p-6 rounded shadow-lg z-20 max-w-2xl w-full">
+          <h1 className="text-5xl font-bold text-center">
             {gameMessage}
           </h1>
         </div>
